7a68812cc7af02229a09060aa9d0962a76eb96ca
LeandroPelegrini <leandropfirmino@gmail.com>
2023-12-20 19:04:10 -0300

    first-commit

---
+++ newsletter-sign-up-with-success-message-main/src/routes/+page.svelte
@@ -0,0 +1,97 @@
+<!-- Sign-up form start -->
+<img src="./images/illustration-sign-up-mobile.svg" alt="">
+
+<article>
+    <h1>
+        Stay updated!
+    </h1>
+    
+    <h2>
+        Join 60,000+ product managers receiving monthly updates on:
+    </h2>
+    
+    <ul>
+        <li>
+            <span>Product discovery and building what matters</span>
+        </li>
+        <li>
+            <span>Measuring to ensure updates are a success</span>
+        </li>
+        <li>
+            <span>And much more!</span>
+        </li>
+    </ul>
+    
+    Email address
+    email@company.com
+    
+    Subscribe to monthly newsletter
+    
+    <!-- Sign-up form end -->
+    
+    <!-- Success message start -->
+    
+    Thanks for subscribing!
+    
+    A confirmation email has been sent to ash@loremcompany.com. 
+    Please open it and click the button inside to confirm your subscription.
+    
+    Dismiss message
+    
+    <!-- Success message end -->
+</article>
+
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+
+
+<style>
+    .test {
+        color: var(--tomato)
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+
+    ul {
+        list-style-image: url('./images/icon-list.svg');
+        
+        margin-left: 2rem;
+    }
+
+    ul li {
+        margin: 1rem 0;
+    }
+
+    ul li img {
+        width: 20px;
+        margin-right: 1rem;
+    }
+
+    article {
+        padding: 1rem;
+        color: var( --darkSlateGrey );
+        max-width: 370px;
+        margin: 0 auto;
+    }
+
+    article h1 {
+        font-size: 2.5rem;
+        padding: 1rem 0;
+    }
+
+    article h2 {
+        font-size: 1rem;
+        font-weight: 400;
+        margin-bottom: 1rem;
+    }
+    @media (min-width: 375px) {
+        * {
+            border: solid red 1px;
+        }
+    }
+</style>
