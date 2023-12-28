d4e9f9f6d8ac3739e2ceaaa88e2a372711e6da33
LeandroPelegrini <leandropfirmino@gmail.com>
2023-12-28 09:49:32 -0300

    modal success

--- newsletter-sign-up-with-success-message-main/src/routes/+page.svelte
+++ newsletter-sign-up-with-success-message-main/src/routes/+page.svelte
@@ -1,15 +1,21 @@
+<script>
+    import Modal from '../lib/components/Modal.svelte';
+
+    let showModal = true;
+    let mail = 'llll@gmail.com'
+</script>
 <!-- Sign-up form start -->
-<img src="./images/illustration-sign-up-mobile.svg" alt="">
+<img class="heroimg" src="./images/illustration-sign-up-mobile.svg" alt="">
 
 <article>
     <h1>
         Stay updated!
     </h1>
-    
+
     <h2>
         Join 60,000+ product managers receiving monthly updates on:
     </h2>
-    
+
     <ul>
         <li>
             <span>Product discovery and building what matters</span>
@@ -21,45 +27,76 @@
             <span>And much more!</span>
         </li>
     </ul>
-    
-    Email address
-    email@company.com
-    
-    Subscribe to monthly newsletter
-    
+
+    <form method="POST">
+        <label>
+            <p>Email address</p>
+            <input bind:value="{mail}" name="email" type="email" placeholder="email@company.com" required>
+        </label>
+        <button on:click={()=> (showModal = true)}>Subscribe to monthly newsletter</button>
+    </form>
+
     <!-- Sign-up form end -->
-    
+
+
+
     <!-- Success message start -->
-    
+
+    <Modal bind:showModal>
+        <div slot="header" class="header">
+            <img class="imgmodal" src="./images/icon-list.svg" alt="">
+            <h1>Thanks for subscribing!</h1>
+        </div>
+
+        <ol class="definition-list">
+            <li>of or relating to modality in logic</li>
+            <li>
+                containing provisions as to the mode of procedure or the manner of taking effect —used of a
+                contract or legacy
+            </li>
+            <li>of or relating to a musical mode</li>
+            <li>of or relating to structure as opposed to substance</li>
+            <li>
+                of, relating to, or constituting a grammatical form or category characteristically indicating
+                predication
+            </li>
+            <li>of or relating to a statistical mode</li>
+        </ol>
+
+        <a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+    </Modal>
+
     Thanks for subscribing!
-    
-    A confirmation email has been sent to ash@loremcompany.com. 
+
+    A confirmation email has been sent to ash@loremcompany.com.
     Please open it and click the button inside to confirm your subscription.
-    
+
     Dismiss message
-    
+
     <!-- Success message end -->
 </article>
 
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
+
+<div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+    Coded by <a href="/">Your Name Here</a>.
+</div>
 
 
 <style>
-    .test {
-        color: var(--tomato)
+    .header img {
+        width: auto;
+        height: 60px;
     }
-    img {
+
+    .heroimg {
         width: 100%;
         height: 100%;
     }
 
     ul {
         list-style-image: url('./images/icon-list.svg');
-        
+
         margin-left: 2rem;
     }
 
@@ -67,14 +104,9 @@
         margin: 1rem 0;
     }
 
-    ul li img {
-        width: 20px;
-        margin-right: 1rem;
-    }
-
     article {
         padding: 1rem;
-        color: var( --darkSlateGrey );
+        color: var(--darkSlateGrey);
         max-width: 370px;
         margin: 0 auto;
     }
@@ -89,6 +121,22 @@
         font-weight: 400;
         margin-bottom: 1rem;
     }
+
+    form p {
+        font-weight: 700;
+    }
+
+    form input {
+        padding: 5px;
+        border-radius: 5px;
+
+    }
+
+    .imgmodal {
+        width: 100%;
+        height: 50px;
+    }
+
     @media (min-width: 375px) {
         * {
             border: solid red 1px;
